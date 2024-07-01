660c135cd25cf1d3d51cb9017029b22a2ab6c3ab
Krunoslav Velfel <61638447+velfel-krunoslav@users.noreply.github.com>
2024-07-01 16:29:16 +0200

    Add files via upload

--- include/common.hpp
+++ include/common.hpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <cstdint>
 #include <vector>
+#include <any>
+#include <memory>
 
 namespace comp
 {
@@ -12,13 +14,20 @@ namespace comp
     {
     public:
         static void calc_prob(std::string, std::map<uint8_t, double> &);
-        static void shannon_fano_encode(const std::string&);
-        static void shannon_fano_decode(const std::string&);
+        static void shannon_fano_encode(const std::string &);
+        static void huffman_encode(const std::string &);
+        static void decode(const std::string &);
+
         static const std::string sf_ext;
+        static const std::string hf_ext;
+
     private:
         struct _node;
         struct _sf_data;
+        static void _write_encoded(std::vector<std::pair<uint8_t, std::vector<bool>>> &, const std::string &, const std::string &);
         static void _shannon_fano(std::vector<struct _sf_data> &, uint8_t, uint8_t, double);
+        static void _huffman_code_gen(std::shared_ptr<comp::common::_node> &, std::vector<bool> &, std::map<uint8_t, std::vector<bool>> &);
+        static std::shared_ptr<comp::common::_node> join_nodes(std::shared_ptr<comp::common::_node>, std::shared_ptr<comp::common::_node>);
     };
 }
 
--- src/common.cpp
+++ src/common.cpp
@@ -10,8 +10,11 @@
 #include <vector>
 #include <memory>
 #include <iomanip>
+#include <queue>
+#include <any>
 
 const std::string comp::common::sf_ext = ".sfef";
+const std::string comp::common::hf_ext = ".hfef";
 
 struct comp::common::_sf_data
 {
@@ -33,22 +36,33 @@ struct comp::common::_sf_data
 
  Leaf nodes are set in all-caps, and hold the corresponding byte.
  */
-struct comp::common::_node
+struct comp::common::_node : public std::enable_shared_from_this<_node>
 {
-    std::map<bool, std::unique_ptr<struct comp::common::_node>> nodes;
+    std::shared_ptr<comp::common::_node> get_shared()
+    {
+        return shared_from_this();
+    };
+
+    std::map<bool, std::shared_ptr<struct comp::common::_node>> nodes;
+
+    _node()
+    {
+        nodes[false] = nullptr;
+        nodes[true] = nullptr;
+    }
 
     bool ends = false;
-    uint8_t decoded_byte;
+    std::any data;
 
     /* TODO: don't modify bits */
-    void insert(std::vector<bool> &bits, uint8_t byte)
+    std::shared_ptr<comp::common::_node> insert(std::vector<bool> &bits, std::any dat)
     {
         // TODO: .empty()?
         if (bits.size() == 0)
         {
-            decoded_byte = byte;
+            data = dat;
             ends = true;
-            return;
+            return get_shared();
         }
 
         const bool next = bits[0];
@@ -60,7 +74,7 @@ struct comp::common::_node
             nodes[next] = std::make_unique<struct comp::common::_node>();
         }
         /* Recursively traverse and keep adding nodes where needed: */
-        nodes[next]->insert(bits, byte);
+        return nodes[next]->insert(bits, dat);
     }
 
     comp::common::_node *find(uint8_t byte, uint8_t &leftover_bits)
@@ -79,54 +93,251 @@ struct comp::common::_node
         leftover_bits--;
         return nodes[next]->find(byte, leftover_bits);
     }
-
-    static std::string byteToHex(uint8_t byte)
-    {
-        std::ostringstream oss;
-        oss << "0x" << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(byte);
-        return oss.str();
-    }
-
-    static void printTrie(const _node *node, const std::string &prefix = "")
-    {
-        if (!node)
-            return;
-
-        // Print current node details
-        std::cout << prefix;
-        if (node->ends)
-        {
-            std::cout << " [END] Decoded Byte: " << byteToHex(node->decoded_byte);
-        }
-        std::cout << std::endl;
-
-        // Recur for true and false branches
-        if (node->nodes.find(false) != node->nodes.end())
-        {
-            std::cout << prefix << "0 -> ";
-            printTrie(node->nodes.at(false).get(), prefix + "  ");
-        }
-
-        if (node->nodes.find(true) != node->nodes.end())
-        {
-            std::cout << prefix << "1 -> ";
-            printTrie(node->nodes.at(true).get(), prefix + "  ");
-        }
-    }
 };
 
-std::string trim_string_ext(const std::string& str) {
+std::string trim_string_ext(const std::string &str)
+{
     std::size_t lastDot = str.find_last_of('.');
 
-    if (lastDot != std::string::npos && lastDot != 0) {
+    if (lastDot != std::string::npos && lastDot != 0)
+    {
         return str.substr(0, lastDot);
     }
 
     return str;
 }
 
-void comp::common::shannon_fano_decode(const std::string &filename)
+void comp::common::_write_encoded(std::vector<std::pair<uint8_t, std::vector<bool>>> &result,
+                                  const std::string &filename, const std::string &output_filename)
 {
+    for (auto &[byte, code] : result)
+    {
+        std::cout << static_cast<int>(byte) << ": ";
+        for (const auto &c : code)
+        {
+            std::cout << c;
+        }
+        std::cout << std::endl;
+    }
+    std::ifstream in(filename, std::ios::binary);
+    std::ofstream out(output_filename, std::ios::binary);
+    /*
+     * Serialize into output_filename:
+     * 1. { prefix bit count | prefix + padding (mod 8 == 0) }, n times, n ASC
+     * 2. { encoded contents }
+     */
+
+    /* 1. */
+    for (auto &[b, vec] : result)
+    {
+        uint8_t prefix_bit_count = static_cast<uint8_t>(vec.size());
+        out.write(reinterpret_cast<char *>(&prefix_bit_count), sizeof prefix_bit_count);
+
+        // TODO: Fix endianness issue!S!
+        uint8_t padded_prefix[256 / (sizeof(uint8_t) * CHAR_BIT)];
+        if (vec.size() == 0)
+        {
+            std::cout << "Fatal error: " << __LINE__ << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        uint8_t len_mod_8 = (((vec.size() - 1) / CHAR_BIT) + 1) * CHAR_BIT;
+
+        int i;
+        const uint8_t mask = 0x1;
+        uint8_t byte = 0x0;
+
+        // When byte resets to 0x0, subsequent bit shift has no ill effect
+        for (i = 0; i < len_mod_8; i++, byte = byte << 1)
+        {
+            if (i < vec.size() && vec[i])
+            {
+
+                byte = byte | mask;
+            }
+            if ((i + 1) % 8 == 0)
+            {
+                out.write(reinterpret_cast<char *>(&byte), sizeof byte);
+                byte = 0x0;
+            }
+        }
+    }
+
+    /* 2. */
+
+    if (!in.is_open())
+    {
+        std::cout << "Failed to open: " << filename << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::map<uint8_t, std::vector<bool>> bitmap(result.begin(), result.end());
+
+    uint8_t in_byte;
+    uint8_t out_byte;
+    uint8_t out_byte_bit_count = 0;
+
+    while (in.read(reinterpret_cast<char *>(&in_byte), sizeof in_byte))
+    {
+        std::vector<bool> prefix_bits = bitmap[in_byte];
+
+        for (bool pb : prefix_bits)
+        {
+            const uint8_t write_mask = (pb) ? 0x1 : 0x0;
+            out_byte = out_byte | write_mask;
+            out_byte_bit_count++;
+
+            if (out_byte_bit_count == 8)
+            {
+                out.write(reinterpret_cast<char *>(&out_byte), sizeof out_byte);
+                out_byte_bit_count = 0;
+                out_byte = 0;
+            }
+            else
+            {
+                out_byte <<= 1;
+            }
+        }
+    }
+
+    /* Flush any remaining bits: */
+    if (out_byte_bit_count)
+    {
+        out_byte <<= (8 - out_byte_bit_count);
+        out.write(reinterpret_cast<char *>(&out_byte), sizeof out_byte);
+    }
+
+    in.close();
+    out.close();
+}
+
+std::shared_ptr<comp::common::_node> comp::common::join_nodes(std::shared_ptr<comp::common::_node> left, std::shared_ptr<comp::common::_node> right)
+{
+    auto tmp = std::make_shared<comp::common::_node>();
+    tmp->nodes[false] = left;
+    tmp->nodes[true] = right;
+
+    /*     auto left_prob = std::any_cast<std::pair<std::vector<uint8_t>, bool>>(tmp->nodes[false]->data).second;
+        auto right_prob = std::any_cast<std::pair<std::vector<uint8_t>, bool>>(tmp->nodes[true]->data).second;
+
+        if(left_prob < right_prob) {
+            auto t = tmp->nodes[false];
+            tmp->nodes[false] = tmp->nodes[true];
+            tmp->nodes[true] = t;
+        } */
+
+    return tmp;
+}
+
+void comp::common::_huffman_code_gen(std::shared_ptr<comp::common::_node> &node, std::vector<bool> &prefix_buffer, std::map<uint8_t, std::vector<bool>> &prefixes)
+{
+    if (std::any_cast<std::pair<std::vector<uint8_t>, double>>(node->data).first.size() == 1)
+    {
+        auto byte = std::any_cast<std::pair<std::vector<uint8_t>, double>>(node->data).first[0];
+        prefixes[byte] = prefix_buffer;
+        return;
+    }
+
+    prefix_buffer.push_back(false);
+    _huffman_code_gen(node->nodes[false], prefix_buffer, prefixes);
+
+    prefix_buffer.pop_back();
+    prefix_buffer.push_back(true);
+    _huffman_code_gen(node->nodes[true], prefix_buffer, prefixes);
+}
+
+void comp::common::huffman_encode(const std::string &filename)
+{
+    std::map<uint8_t, double> prob;
+    comp::common::calc_prob(filename, prob);
+
+    struct cmpPair
+    {
+        bool operator()(const std::shared_ptr<_node> &a, const std::shared_ptr<_node> &b) const
+        {
+            auto first = std::any_cast<std::pair<std::vector<uint8_t>, double>>(a->data);
+            auto second = std::any_cast<std::pair<std::vector<uint8_t>, double>>(b->data);
+
+            return first.second < second.second;
+        }
+    } cmp;
+
+    std::priority_queue<std::shared_ptr<_node>, std::vector<std::shared_ptr<_node>>, cmpPair> result;
+
+    for (auto &[byte, pi] : prob)
+    {
+        auto new_node = std::make_shared<_node>();
+        new_node->data = std::make_pair(std::vector<uint8_t>{byte}, pi);
+        result.push(new_node);
+    }
+
+    /* while (result.size())
+    {
+        auto pair = std::any_cast<std::pair<std::vector<uint8_t>, double>>(result.top()->data);
+
+        std::cout << static_cast<int>(std::any_cast<std::vector<uint8_t>>(pair.first)[0]) << std::endl;
+        std::cout << std::any_cast<double>(pair.second) << std::endl;
+        result.pop();
+    } */
+
+    while (result.size() != 1)
+    {
+        std::shared_ptr<_node> data[2];
+
+        for (int i = 0; i < 2; i++)
+        {
+            data[i] = result.top();
+            result.pop();
+        }
+
+        /* Flip order: */
+        auto new_node = comp::common::join_nodes(data[1], data[0]);
+
+        auto left_data = std::any_cast<std::pair<std::vector<uint8_t>, double>>(data[0]->data);
+        auto right_data = std::any_cast<std::pair<std::vector<uint8_t>, double>>(data[1]->data);
+
+        std::vector<uint8_t> tmp;
+        tmp.reserve(left_data.first.size() + right_data.first.size());
+
+        tmp.insert(tmp.end(), left_data.first.begin(), left_data.first.end());
+        tmp.insert(tmp.end(), right_data.first.begin(), right_data.first.end());
+
+        new_node->data = std::make_pair(tmp, left_data.second + right_data.second);
+        result.push(new_node);
+    }
+
+    auto root = result.top();
+
+    std::map<uint8_t, std::vector<bool>> codes;
+    std::vector<bool> buf;
+
+    auto tree = std::make_shared<comp::common::_node>();
+    _huffman_code_gen(root, buf, codes);
+    for (auto &[byte, code] : codes)
+    {
+
+        /*Remove leading zeroes:*/
+        while ((code.size() - 1) && code.front() != 1)
+        {
+            code.erase(code.begin());
+        }
+        std::cout << static_cast<int>(byte) << ": ";
+        for (const auto &c : code)
+        {
+            std::cout << c;
+        }
+        std::cout << std::endl;
+    }
+
+    std::vector<std::pair<uint8_t, std::vector<bool>>> _result(codes.begin(), codes.end());
+
+    const std::string output_filename = filename + hf_ext;
+
+    _write_encoded(_result, filename, output_filename);
+}
+
+void comp::common::decode(const std::string &filename)
+{
+    // TODO what if the decoded filename already exists?
     std::ifstream in(filename, std::ios::binary);
     std::ofstream out(trim_string_ext(filename), std::ios::binary);
     std::cout << trim_string_ext(filename) << std::endl;
@@ -165,17 +376,19 @@ void comp::common::shannon_fano_decode(const std::string &filename)
             }
         }
 
-        /* printf("%5d: ", i);
-        for(int j = 0; j < bits.size(); j++) {
+        printf("%5d: ", i);
+        for (int j = 0; j < bits.size(); j++)
+        {
             printf("%c", (bits[j]) ? '1' : '0');
         }
-        printf("\n"); */
+        printf("\n");
         trie->insert(bits, static_cast<uint8_t>(i));
-
     }
 
     uint8_t leftover = 0;
     _node *t = trie.get();
+
+    /* comp::common::_node::printTrie(t, ""); */
 
     for (;;)
     {
@@ -197,7 +410,7 @@ void comp::common::shannon_fano_decode(const std::string &filename)
 
         if (t->ends)
         {
-            out.write(reinterpret_cast<char*>(&(t->decoded_byte)), sizeof t->decoded_byte);
+            out.write(reinterpret_cast<char *>(&(t->data)), sizeof t->data);
             t = trie.get();
         }
     }
@@ -245,7 +458,7 @@ void comp::common::shannon_fano_encode(const std::string &filename)
 
     comp::common::_shannon_fano(dat, l, r, 1);
 
-    for (_sf_data &t : dat)
+    for (_sf_data t : dat)
     {
         result.push_back(std::pair<uint8_t, std::vector<bool>>(t.byte, t.prefix));
     }
@@ -264,95 +477,7 @@ void comp::common::shannon_fano_encode(const std::string &filename)
 
     const std::string output_filename = filename + comp::common::sf_ext;
 
-    std::ofstream out(output_filename, std::ios::binary);
-
-    /*
-     * Serialize into output_filename:
-     * 1. { prefix bit count | prefix + padding (mod 8 == 0) }, n times, n ASC
-     * 2. { encoded contents }
-     */
-
-    /* 1. */
-    for (auto &[b, vec] : result)
-    {
-        uint8_t prefix_bit_count = static_cast<uint8_t>(vec.size());
-        out.write(reinterpret_cast<char *>(&prefix_bit_count), sizeof prefix_bit_count);
-
-        // TODO: Fix endianness issue!S!
-        uint8_t padded_prefix[256 / (sizeof(uint8_t) * CHAR_BIT)];
-        if (vec.size() == 0)
-        {
-            std::cout << "Fatal error: " << __LINE__ << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        uint8_t len_mod_8 = (((vec.size() - 1) / CHAR_BIT) + 1) * CHAR_BIT;
-
-        int i;
-        const uint8_t mask = 0x1;
-        uint8_t byte = 0x0;
-
-        // When byte resets to 0x0, subsequent bit shift has no ill effect
-        for (i = 0; i < len_mod_8; i++, byte = byte << 1)
-        {
-            if (i < vec.size() && vec[i])
-            {
-                byte = byte | mask;
-            }
-            if ((i + 1) % 8 == 0)
-            {
-                out.write(reinterpret_cast<char *>(&byte), sizeof byte);
-                byte = 0x0;
-            }
-        }
-    }
-
-    /* 2. */
-    std::ifstream in(filename, std::ios::binary);
-
-    if (!in.is_open())
-    {
-        std::cout << "Failed to open: " << filename << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    std::map<uint8_t, std::vector<bool>> bitmap(result.begin(), result.end());
-
-    uint8_t in_byte;
-    uint8_t out_byte;
-    uint8_t out_byte_bit_count = 0;
-
-    while (in.read(reinterpret_cast<char *>(&in_byte), sizeof in_byte))
-    {
-        std::vector<bool> prefix_bits = bitmap[in_byte];
-
-        for (bool pb : prefix_bits)
-        {
-            const uint8_t write_mask = (pb) ? 0x1 : 0x0;
-            out_byte = out_byte | write_mask;
-            out_byte_bit_count++;
-
-            if (out_byte_bit_count == 8)
-            {
-                out.write(reinterpret_cast<char *>(&out_byte), sizeof out_byte);
-                out_byte_bit_count = 0;
-                out_byte = 0;
-            }
-            else
-            {
-                out_byte <<= 1;
-            }
-        }
-    }
-
-    /* Flush any remaining bits: */
-    if (out_byte_bit_count)
-    {
-        out_byte <<= (8 - out_byte_bit_count);
-        out.write(reinterpret_cast<char *>(&out_byte), sizeof out_byte);
-    }
-
-    in.close();
-    out.close();
+    _write_encoded(result, filename, output_filename);
 }
 
 /* Split + build prefix */
