9281ad2e666083038d8c08654d02814aeb64fef4
Krunoslav Velfel <61638447+velfel-krunoslav@users.noreply.github.com>
2024-07-07 17:25:48 +0200

    Add files via upload

--- src/1_3_lz77.cpp
+++ src/1_3_lz77.cpp
@@ -3,11 +3,7 @@
 #include <cstdlib>
 #include <memory>
 #include <vector>
-
-#define SEARCH_BUF_SIZE 64
-#define LOOKAHEAD_BUF_SIZE 192
-
-static_assert(SEARCH_BUF_SIZE + LOOKAHEAD_BUF_SIZE == 256, "Adjust buffer sizes");
+#include <fstream>
 
 class Buffer
 {
@@ -76,17 +72,18 @@ struct out
 
 void largest_repeating_sequence(Buffer &search, Buffer &lookahead, struct out &result)
 {
-    for (int i = 0; i < search.size; i++)
-    {
-        std::cout << search.at(i) << " ";
-    }
-    std::cout << "| ";
+    /*     for (int i = 0; i < search.size; i++)
+        {
+            std::cout << search.at(i) << " ";
+        }
+        std::cout << "| ";
 
-    for (int i = 0; i < lookahead.size; i++)
-    {
-        std::cout << lookahead.at(i) << " ";
-    }
-    std::cout << std::endl;
+        for (int i = 0; i < lookahead.size; i++)
+        {
+            std::cout << lookahead.at(i) << " ";
+        }
+        std::cout << std::endl; */
+
     /* First try to find the largest substring from the lookahead buffer.
      * subsize - length of the lookahead substring to find
      */
@@ -96,16 +93,16 @@ void largest_repeating_sequence(Buffer &search, Buffer &lookahead, struct out &r
         for (int i = search.size - 1; i >= 0; i--)
         {
             bool matching = true;
-            int j = i;
+            int j;
 
             /* Search starting from the search buffer... */
-            for (; matching && j < search.size; j++)
+            for (j = i; matching && j < search.size && j < i + subsize; j++)
             {
                 matching = matching && (search.at(j) == lookahead.at(j - i));
             }
 
             /* ...and into the lookahead buffer if possible: */
-            for (j = 0; matching && j < subsize - (search.size - i); j++)
+            for (j = 0; matching && j < i + subsize - search.size; j++)
             {
                 matching = matching && (lookahead.at(j) == lookahead.at(j + (search.size - i)));
             }
@@ -114,44 +111,78 @@ void largest_repeating_sequence(Buffer &search, Buffer &lookahead, struct out &r
             if (matching)
             {
                 result = {.bytes = {static_cast<uint8_t>(i), subsize, lookahead.at(subsize)}};
-                printf("<%d, %d, %c>\n\n", result.bytes[0], result.bytes[1], result.bytes[2]);
                 return;
             }
         }
     }
     /* Otherwise, a unique byte has been encountered, add it to the search buffer.
      *
-     * Additionally, this case is triggered if all the remaining bytes in the lookahead buffer form
+     * Additionally, this case is triggered if all bytes in the lookahead buffer form
      * a substring somewhere in the search buffer. Example:
-     * 
-     *       SEARCH                LOOKAHEAD
-     * 
-     * a0, a1, a2, a3, ..., an | a1, a2, a3, eof
-     * 
-     * a1, a2 will be caught by the nested loops above, whereas a3 will be re-added as if was a distinct search buffer byte.
-     * This does weaken the compression ratio, but by a negligible amount.
+     *
+     *       SEARCH[n+1]         LOOKAHEAD[3]
+     *
+     * a0, a1, a2, a3, ..., an | a1, a2, a3
+     *
+     * a1, a2 will be caught by the nested loops above, whereas a3 will be re-added to the search buffer as if was distinct,
+     * regardless if it already exists in the search buffer. This does weaken the compression ratio, but only by a negligible amount.
      */
     result = {.bytes = {0, 0, lookahead.at(0)}};
-    printf("<%d, %d, %c>\n\n", result.bytes[0], result.bytes[1], result.bytes[2]);
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    Buffer search(4), lookahead(4);
+    if (argc < 3)
+    {
+        std::cout << "Usage: {-e|-d} <filename>" << std::endl;
+        return EXIT_FAILURE;
+    }
 
-    uint8_t val[] = {'A', 'B', 'A', 'B', 'C', 'B', 'C', 'B', 'A', 'A', 'B', 'C', 'A', 'B', 'B'};
+    const std::string mode(argv[1]);
+    const std::string filename(argv[2]);
+
+    static const int search_buffer_size = 64;
+    static const int lookahead_buffer_size = 192;
+
+    static_assert(search_buffer_size + lookahead_buffer_size == 256, "Adjust buffer sizes");
+    
+    if (mode == "-e")
+    {
+        /* Encode the file: */
+
+    }
+    else if (mode == "-d")
+    {
+        /* Decode the file: */
+
+    }
+    else
+    {
+        std::cout << "Usage: {-e|-d} <filename>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+
+    Buffer search(search_buffer_size), lookahead(lookahead_buffer_size);
 
     uint16_t i;
-    for (i = 0; i < 4; i++)
+    for (i = 0; i < lookahead_buffer_size; i++)
     {
         lookahead.push(val[i % 10]);
     }
+
+    std::vector<struct out> encoded;
+    std::vector<uint8_t> decoded;
 
     while (lookahead.size)
     {
         struct out result;
 
         largest_repeating_sequence(search, lookahead, result);
+
+        encoded.push_back(result);
+
+        printf("%2d,%2d,%2c\n", result.bytes[0], result.bytes[1], result.bytes[2]);
 
         uint8_t len = result.bytes[1];
 
@@ -168,6 +199,31 @@ int main(void)
             search.push(pop);
         }
     }
+
+    for (auto &t : encoded)
+    {
+        if (t.bytes[1])
+        {
+            int index_shift = decoded.size() - 4;
+
+            if (index_shift < 0)
+            {
+                index_shift = 0;
+            }
+            for (int i = t.bytes[0]; i < t.bytes[0] + t.bytes[1]; i++)
+            {
+                decoded.push_back(decoded[index_shift + i]);
+            }
+        }
+        decoded.push_back(t.bytes[2]);
+    }
+
+    for (auto t : decoded)
+    {
+        std::cout << t << " ";
+    }
+
+    std::cout << std::endl;
 
     return EXIT_SUCCESS;
 }
